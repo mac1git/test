a466868f55b054f44f1cce1af5d3056dc08a1943
mac1git <haruki.nakamura@eparkdock.com>
2019-09-04 10:49:25 +0900

    Controller から View へ変数を渡す

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,19 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function about() {
+    public function about()
+    {
+        // 配列に値をセット
+        $data = [
+            'first_name' => 'Luke',
+            'last_name' => 'Skywaler'
+            ];
+ 
+        // view関数の第２引数に配列を渡す
+        return view('pages.about', $data);        
+ 
+        /* 以前のコード
         return view('pages.about');
+        */
     }
 }
