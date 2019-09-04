23fde6edf931d2d116893c2f7cfb4e7e7e794d80
mac1git <haruki.nakamura@eparkdock.com>
2019-09-04 10:58:19 +0900

    HTML をごっそりと消して、layout ビューに移す

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,11 +1,7 @@
 <!-- about.blade.php -->
  
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>About</title>
-</head>
-<body>
+@extends('layout')
+ 
+@section('content')
     <h1>About Me: {{ $first_name }} {{ $last_name }}</h1>
-</body>
-</html>
+@endsection
